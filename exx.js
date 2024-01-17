fc1d01daa64c3c9657a760bcefc294c91ff088b5
ajaybhaskar <ajaybhaskar@lpu.in>
2024-01-17 15:02:35 +0530

    version 1

---
+++ exx.js
@@ -0,0 +1,4 @@
+// JavaScript code for a personalized greeting
+let userName = prompt("What is your name?");
+let greeting = `Hello, ${userName}! Welcome to JavaScript programming.`;
+alert(greeting);
